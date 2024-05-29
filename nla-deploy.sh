1d8cdbc3fa5325fa07c29dc3b59629191399a608
Alex Osborne <aosborne@nla.gov.au>
2024-05-29 16:17:36 +0900

    nla-deploy.sh: Correct argument order for unzip

--- nla-deploy.sh
+++ nla-deploy.sh
@@ -3,7 +3,7 @@ mvn package dependency:copy-dependencies
 mkdir "$1"/{lib,content}
 cp odin-server/target/*.jar odin-server/target/dependency/* "$1"/lib
 cp -R odin-server/config/ "$1"/config/
-unzip -d odin-ui/target/*.war "$1"/content
+unzip -d "$1"/content odin-ui/target/*.war
 
 # FIXME: make the app look at environment variables
 sed -i 's/^\(loki.http.request.adapter.port = \).*/\1'"$PORT/" "$1"/config/http_adapter.properties
