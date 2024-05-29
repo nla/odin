00990f3be240fbad071c3be12748eb664282bab6
nla-manderson <168795533+nla-manderson@users.noreply.github.com>
2024-05-29 10:54:47 +1000

    Formatting fix for Document.java

--- loki-ui/src/main/java/loki/ui/Document.java
+++ loki-ui/src/main/java/loki/ui/Document.java
@@ -8,44 +8,44 @@ import com.google.gwt.core.client.JavaScriptObject;
 
 public class Document
 {
-	private JavaScriptObject reference;
-	
-	private Document(JavaScriptObject reference)
-	{
-		this.reference = reference;
-	}
-	
-	/**
-	 * Returns the body element of this document.
-	 * 
-	 * @return     the body element of this document.
-	 */
-	
-	public Entity getBody()
-	{
-		Entity body = new Entity();
-		body.setReference(getBodyElementReference(reference));
-		return body;
-	}
-	
-	private native JavaScriptObject getBodyElementReference(JavaScriptObject document)
-	/*-{
-	  return document.body;
-	}-*/;
-	
-	/**
+    private JavaScriptObject reference;
+
+    private Document(JavaScriptObject reference)
+    {
+        this.reference = reference;
+    }
+
+    /**
+     * Returns the body element of this document.
+     * 
+     * @return the body element of this document.
+     */
+
+    public Entity getBody()
+    {
+        Entity body = new Entity();
+        body.setReference(getBodyElementReference(reference));
+        return body;
+    }
+
+    private native JavaScriptObject getBodyElementReference(JavaScriptObject document)
+    /*-{
+      return document.body;
+    }-*/;
+
+    /**
      * A static method to returns root document of the context this application is running in.
      * 
-     * @return     the root document.
+     * @return the root document.
      */
-	
-	public static Document getRootDocument()
-	{
-		return new Document(getRootDocumentReference());
-	}
-	
-	private static native JavaScriptObject getRootDocumentReference()
-	/*-{
-	  return $doc;
-	}-*/;
+
+    public static Document getRootDocument()
+    {
+        return new Document(getRootDocumentReference());
+    }
+
+    private static native JavaScriptObject getRootDocumentReference()
+    /*-{
+      return $doc;
+    }-*/;
 }
