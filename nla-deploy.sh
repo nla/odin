16d0ab8bc58c1baa697a9b7ecaf7cc96f736a732
Alex Osborne <aosborne@nla.gov.au>
2024-05-29 16:13:13 +0900

    Configure PORT and HOST

--- nla-deploy.sh
+++ nla-deploy.sh
@@ -4,3 +4,7 @@ mkdir "$2"/{lib,content}
 cp odin-server/target/*.jar odin-server/target/dependency/* "$2"/lib
 cp -R odin-server/config/ "$2"/config/
 unzip -d odin-ui/target/*.war "$2"/content
+
+# FIXME: make the app look at environment variables
+sed -i 's/^\(loki.http.request.adapter.port = \).*/\1'"$PORT/" "$2"/config/http_adapter.properties
+sed -i 's/^\(loki.http.request.adapter.host = \).*/\1'"${HOST:-127.0.0.1}/" "$2"/config/http_adapter.properties
