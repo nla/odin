41b11baf75a03e0187cb133d4c0891d123a6e6f5
nla-manderson <168795533+nla-manderson@users.noreply.github.com>
2024-05-29 10:56:29 +1000

    Formatting fix for Entity.java

--- loki-ui/src/main/java/loki/ui/Entity.java
+++ loki-ui/src/main/java/loki/ui/Entity.java
@@ -8,85 +8,85 @@ import com.google.gwt.core.client.JavaScriptObject;
 
 public class Entity
 {
-	private JavaScriptObject reference;
+    private JavaScriptObject reference;
 
-	protected Entity(){}
-	
-	protected void setReference(JavaScriptObject reference)
-	{
-		this.reference = reference;
-	}
-	
-	/**
-	 * Returns a reference to the underlying JS object.
-	 */
+    protected Entity(){}
 
-	public JavaScriptObject getReference()
-	{
-		return reference;
-	}
-	
-	/**
+    protected void setReference(JavaScriptObject reference)
+    {
+        this.reference = reference;
+    }
+
+    /**
+     * Returns a reference to the underlying JS object.
+     */
+
+    public JavaScriptObject getReference()
+    {
+        return reference;
+    }
+
+    /**
      * Adds the specified list of elements to this entity as child nodes.
      * 
-     * @param elements       The list of elements to add as child nodes.
+     * @param elements The list of elements to add as child nodes.
      */
-	
-	public void add(Element... elements)
-	{
-	    for(Element element: elements)
-	    {
-	        addElement(reference, element.getReference());
-	    }
-	}
-	
-	private native void addElement(JavaScriptObject parent, JavaScriptObject child)
-	/*-{
+
+    public void add(Element... elements)
+    {
+        for(Element element: elements)
+        {
+            addElement(reference, element.getReference());
+        }
+    }
+
+    private native void addElement(JavaScriptObject parent, JavaScriptObject child)
+    /*-{
         parent.appendChild(child);
-	}-*/;
-	
-	/**
+    }-*/;
+
+    /**
      * Removes the specified child element.
      * 
-     * @param element       The element to use.
+     * @param element The element to use.
      */
-	
-	public void remove(Element element)
+
+    public void remove(Element element)
     {
         removeElement(reference, element.getReference());
     }
-	
-	private native void removeElement(JavaScriptObject parent, JavaScriptObject child)
+
+    private native void removeElement(JavaScriptObject parent, JavaScriptObject child)
     /*-{
         parent.removeChild(child);
     }-*/;
-	
-	/**
+
+    /**
      * Removes this element from its parent, if it has one.
      */
-	
-	public void removeSelf()
+
+    public void removeSelf()
     {
         removeSelf(reference);
     }
-	
-	private native void removeSelf(JavaScriptObject child)
+
+    private native void removeSelf(JavaScriptObject child)
     /*-{
         if(child.parentNode)
         {
             child.parentNode.removeChild(child);
         }
     }-*/;
-	
-	/**
+
+    /**
      * Removes all child elements.
      */
-	
-	public void clear()
+
+    public void clear()
     {
-	    clear(reference);
+        clear(reference);
     }
-    
+
     private native void clear(JavaScriptObject parent)
     /*-{
         while(parent.firstChild)
