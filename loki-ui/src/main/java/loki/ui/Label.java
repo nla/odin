88abd143114b4a735f4f6455cafd4f11feafbaaf
nla-manderson <168795533+nla-manderson@users.noreply.github.com>
2024-05-29 10:57:15 +1000

    Formatting fix for Label.java

--- loki-ui/src/main/java/loki/ui/Label.java
+++ loki-ui/src/main/java/loki/ui/Label.java
@@ -9,34 +9,34 @@ public class Label extends Element
     /**
      * Creates a new DIV element with the specified text as inner HTML.
      * 
-     * @param text      The text to add to this DIV.
+     * @param text The text to add to this DIV.
      */
-    
-	public Label(String text)
-	{
-		super("div");
-		setText(text);
-	}
-	
-	/**
-	 * Returns the text of this element, obtained from its inner HTML.
-	 * 
-	 * @return         the text of this element.
-	 */
-	
-	public String getText()
-	{
-		return getInnerHTML();
-	}
-	
-	/**
+
+    public Label(String text)
+    {
+        super("div");
+        setText(text);
+    }
+
+    /**
+     * Returns the text of this element, obtained from its inner HTML.
+     * 
+     * @return the text of this element.
+     */
+
+    public String getText()
+    {
+        return getInnerHTML();
+    }
+
+    /**
      * Replaces the text of this element with the specified text as inner HTML.
      * 
-     * @param text      The text to set.
+     * @param text The text to set.
      */
-	
-	public void setText(String text)
-	{
-		setInnerHTML(text);
-	}
+
+    public void setText(String text)
+    {
+        setInnerHTML(text);
+    }
 }
