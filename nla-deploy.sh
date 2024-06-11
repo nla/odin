e003cd5336ecf0868e8e6e59f92c7892d978343e
nla-manderson <168795533+nla-manderson@users.noreply.github.com>
2024-06-11 15:46:08 +1000

    Update nla-deploy.sh

--- nla-deploy.sh
+++ nla-deploy.sh
@@ -6,7 +6,7 @@ mkdir "$1"/{lib,content,content/odin-ui}
 cp odin-server/target/*.jar odin-server/target/dependency/* "$1"/lib
 cp -R odin-server/config/ "$1"/config/
 cp -R odin-ui/war/* "$1"/content/
-cp odin-ui-compile/target/odin-ui-*/odin-ui/* "$1"/content/odin-ui/
+cp odin-ui-compile/target/odin-ui-*/odin_ui/* "$1"/content/odin-ui/
 
 mkdir "$1"/content/root/
 mv "$1"/content/index.html "$1"/content/root/index.html
