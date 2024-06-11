8d45eeb28628b613e48e64f29f35c2e6078f71f5
nla-manderson <168795533+nla-manderson@users.noreply.github.com>
2024-06-11 15:45:18 +1000

    Update nla-deploy.sh

--- nla-deploy.sh
+++ nla-deploy.sh
@@ -2,10 +2,11 @@
 JAVA_HOME=/usr/lib/jvm/java-11-openjdk
 mvn package dependency:copy-dependencies
 
-mkdir "$1"/{lib,content}
+mkdir "$1"/{lib,content,content/odin-ui}
 cp odin-server/target/*.jar odin-server/target/dependency/* "$1"/lib
 cp -R odin-server/config/ "$1"/config/
 cp -R odin-ui/war/* "$1"/content/
+cp odin-ui-compile/target/odin-ui-*/odin-ui/* "$1"/content/odin-ui/
 
 mkdir "$1"/content/root/
 mv "$1"/content/index.html "$1"/content/root/index.html
