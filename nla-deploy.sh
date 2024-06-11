55088ec87d27cd07c2035841cb75666a7f24a545
nla-manderson <168795533+nla-manderson@users.noreply.github.com>
2024-06-11 15:47:48 +1000

    Update nla-deploy.sh

--- nla-deploy.sh
+++ nla-deploy.sh
@@ -2,13 +2,11 @@
 JAVA_HOME=/usr/lib/jvm/java-11-openjdk
 mvn package dependency:copy-dependencies
 
-mkdir "$1"/{lib,content,content/odin-ui}
+mkdir "$1"/{lib,content,content/odin_ui, content/root}
 cp odin-server/target/*.jar odin-server/target/dependency/* "$1"/lib
 cp -R odin-server/config/ "$1"/config/
 cp -R odin-ui/war/* "$1"/content/
-cp odin-ui-compile/target/odin-ui-*/odin_ui/* "$1"/content/odin-ui/
-
-mkdir "$1"/content/root/
+cp odin-ui-compile/target/odin-ui-*/odin_ui/* "$1"/content/odin_ui/
 mv "$1"/content/index.html "$1"/content/root/index.html
 
 # FIXME: make the app look at environment variables
