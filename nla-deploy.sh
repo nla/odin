df2251002bcaa16c6550b08951e46e636bb61d90
nla-manderson <168795533+nla-manderson@users.noreply.github.com>
2024-06-11 15:13:22 +1000

    Update nla-deploy.sh

--- nla-deploy.sh
+++ nla-deploy.sh
@@ -4,7 +4,7 @@ mvn package dependency:copy-dependencies
 mkdir "$1"/{lib,content}
 cp odin-server/target/*.jar odin-server/target/dependency/* "$1"/lib
 cp -R odin-server/config/ "$1"/config/
-cp -R odin-ui/war/ "$1"/content/
+cp -R odin-ui/war/* "$1"/content/
 
 mkdir "$1"/content/root/
 mv "$1"/content/index.html "$1"/content/root/index.html
